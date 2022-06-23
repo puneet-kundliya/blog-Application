61c893f5b82ff1b13eee20c070688acb8c67ad6c
Puneet_Kundliya <puneet.kundliya@gmail.com>
2022-06-23 18:35:45 +0530

    files

---
+++ src/test/java/com/blogpost/project/BlogpostProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogpost.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpostProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
