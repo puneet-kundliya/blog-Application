ae9377f8989098b0ad3038e7323b5bfa919fe952
Puneet_Kundliya <puneet.kundliya@gmail.com>
2022-06-23 17:31:14 +0530

    Main controller springboot

---
+++ src/main/java/com/blogpost/project/BlogpostProjectApplication.java
@@ -0,0 +1,11 @@
+package com.blogpost.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpostProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpostProjectApplication.class, args);
+	}
+}
