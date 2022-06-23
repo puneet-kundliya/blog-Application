b9619350a166c17a0355150ec1ea11916eb66afc
Puneet_Kundliya <puneet.kundliya@gmail.com>
2022-06-23 17:34:49 +0530

    this is tag repo extending JPA repo

---
+++ src/main/java/com/blogpost/project/repository/TagRepository.java
@@ -0,0 +1,11 @@
+package com.blogpost.project.repository;
+
+
+import com.blogpost.project.model.Tags;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tags, Integer> {
+
+}
