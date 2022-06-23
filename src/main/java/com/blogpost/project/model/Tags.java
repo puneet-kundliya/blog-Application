ad1cca9714b39b83cd46d51483618159428d33af
Puneet_Kundliya <puneet.kundliya@gmail.com>
2022-06-23 17:33:46 +0530

    this class is POJO class for Tags data

---
+++ src/main/java/com/blogpost/project/model/Tags.java
@@ -0,0 +1,40 @@
+package com.blogpost.project.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tags")
+public class Tags {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
