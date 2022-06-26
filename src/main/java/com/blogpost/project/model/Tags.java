3a8a2e9aa607e8bc212af1386bb07a0be7c7c245
Puneet_Kundliya <puneet.kundliya@gmail.com>
2022-06-27 00:16:08 +0530

    .

--- src/main/java/com/blogpost/project/model/Tags.java
+++ src/main/java/com/blogpost/project/model/Tags.java
@@ -2,6 +2,8 @@ package com.blogpost.project.model;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tags")
@@ -15,26 +17,40 @@ public class Tags {
     @Column(name = "created_at")
     private Timestamp createdAt;
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "post_tags",
+            joinColumns = @JoinColumn(name = "tag_id"),
+            inverseJoinColumns = @JoinColumn(name = "post_id"))
+    private List<Posts> posts=new ArrayList<>();
+
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Timestamp getCreatedAt() {
         return createdAt;
     }
     public void setCreatedAt(Timestamp createdAt) {
         this.createdAt = createdAt;
+    }
+    public List<Posts> getPosts() {
+        return posts;
+    }
+    public void setPosts(List<Posts> posts) {
+        this.posts = posts;
+    }
+    public void addPost(Posts thePost){
+        if(posts ==null){
+            posts = new ArrayList<>();
+        }
+        posts.add(thePost);
     }
 }
