a9acb82cd091741aca28201caefe1507219a375e
Puneet_Kundliya <puneet.kundliya@gmail.com>
2022-06-27 00:23:44 +0530

    comment service interface created with method to implement

---
+++ src/main/java/com/blogpost/project/service/CommentService.java
@@ -0,0 +1,14 @@
+package com.blogpost.project.service;
+
+import com.blogpost.project.model.Comments;
+
+public interface CommentService {
+
+    void saveComment(Comments comments);
+
+    void deleteComment(Comments comment);
+
+    Comments getCommentById(Integer commentId);
+
+    void  updateComments(Comments comment, Comments oldComment);
+}
