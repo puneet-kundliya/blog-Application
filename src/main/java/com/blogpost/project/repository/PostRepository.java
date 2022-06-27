57199adcf0e27bd1991e380e04a2bbb5b665aa95
Puneet_Kundliya <puneet.kundliya@gmail.com>
2022-06-27 12:00:14 +0530

    added a method for searching in post repo

--- src/main/java/com/blogpost/project/repository/PostRepository.java
+++ src/main/java/com/blogpost/project/repository/PostRepository.java
@@ -2,9 +2,15 @@ package com.blogpost.project.repository;
 
 import com.blogpost.project.model.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Posts, Integer> {
 
+    @Query(value = "SELECT * FROM public.posts post WHERE post.title LIKE  %:keyword%  OR post.content LIKE %:keyword%",nativeQuery = true)
+    List<Posts> findPostBySearch(@Param("keyword") String keyword);
 }
