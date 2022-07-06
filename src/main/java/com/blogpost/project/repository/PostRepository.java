658a286727424f3d4bd7a958e9e58ac58c815292
Puneet_Kundliya <puneet.kundliya@gmail.com>
2022-07-06 16:06:41 +0530

    Added serch by author also

--- src/main/java/com/blogpost/project/repository/PostRepository.java
+++ src/main/java/com/blogpost/project/repository/PostRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Posts, Integer> {
 
-    @Query(value = "select * from public.posts post where lower(post.title) like %?1% OR lower(post.content) LIke %?1% OR post.id in (select id from post_tags inner Join tags on tags.id = post_tags.tag_id where lower(tags.name) like %?1%)",nativeQuery = true)
+    @Query(value = "select * from public.posts post where lower(post.title) like %?1% OR lower(post.content) LIke %?1% OR lower(post.author) LIke %?1% OR post.id in (select id from post_tags inner Join tags on tags.id = post_tags.tag_id where lower(tags.name) like %?1%)",nativeQuery = true)
     Page<Posts> findBySearch(String keyword, Pageable pageable);
 
     @Query(value = "select * from posts where id in (select p.id from posts p join post_tags pt on p.id = pt.post_id join tags t on t.id = pt.tag_id where t.id in :selectedTags)", nativeQuery = true)
