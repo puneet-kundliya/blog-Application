aa54d839d333a03e8d45d0a0cf46734eac3d1d27
Puneet_Kundliya <puneet.kundliya@gmail.com>
2022-07-09 15:12:32 +0530

    Changes in the properties file for heroku database

--- src/main/java/com/blogpost/project/Serviceimplementation/PostServiceImpl.java
+++ src/main/java/com/blogpost/project/Serviceimplementation/PostServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import java.sql.Timestamp;
 import java.util.ArrayList;
